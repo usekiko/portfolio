eac296a2f8e979868db6ae6992b1a03f8009bdc9
usekiko <id4rp02s@gmail.com>
2026-09-22 17:38:57 +0200

    shorter banner and tighter header card

--- app/header.tsx
+++ app/header.tsx
@@ -4,7 +4,7 @@ import { AVATAR_IMAGE, BANNER_IMAGE, BANNER_VIDEO } from './data'
 export function Header() {
   return (
     <header className="mb-12 overflow-hidden rounded-3xl border border-zinc-900 bg-black">
-      <div className="relative h-56 bg-zinc-900">
+      <div className="relative h-40 bg-zinc-900">
         {/* pointer-events-none keeps it from being paused, hovered for controls or right-clicked */}
         <video
           src={BANNER_VIDEO}
@@ -20,19 +20,19 @@ export function Header() {
           tabIndex={-1}
           className="pointer-events-none absolute inset-0 h-full w-full object-cover select-none"
         />
-        <div className="absolute inset-x-0 bottom-0 h-20 bg-linear-to-b from-transparent to-black" />
+        <div className="absolute inset-x-0 bottom-0 h-16 bg-linear-to-b from-transparent to-black" />
       </div>
-      <div className="relative -mt-16 flex flex-col items-center pb-5 text-center">
+      <div className="relative -mt-14 flex flex-col items-center pb-4 text-center">
         <div className="mask-squircle bg-black p-2">
           <img
             src={AVATAR_IMAGE}
             alt="Kiko"
-            className="h-28 w-28 mask-squircle object-cover"
+            className="h-24 w-24 mask-squircle object-cover"
             loading="eager"
             decoding="sync"
           />
         </div>
-        <div className="mt-2">
+        <div className="mt-1">
           <Link href="/" className="text-xl font-normal text-white">
             Kiko
           </Link>
